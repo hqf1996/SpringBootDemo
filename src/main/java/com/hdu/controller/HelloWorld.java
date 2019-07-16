c32b90abaca93d97704307a699a52809f3740985
hqf1996 <378938941@qq.com>
2019-07-17 01:03:43 +0800

    branch test

--- src/main/java/com/hdu/controller/HelloWorld.java
+++ src/main/java/com/hdu/controller/HelloWorld.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world!";
     }
 }
